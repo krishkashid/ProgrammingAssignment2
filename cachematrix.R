82e47c6a20b6ae2203967a8da2a5d8f47fbc0fd1
krishkashid <krish.kashid@gmail.com>
2015-09-25 22:37:01 +0530

    Programming assignment 2
    Cache matrix will check for inverse of matrix in cache memory before
    computing it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program will search for previously chached inverse of
+## matrix before calculating the inverse of matrix
 
-## Write a short comment describing this function
+## This is function used to cache set and get inverted function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) m <<- inv
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function is used to check whether inverse of matrix is available
+## or not, if available call get function
 
 cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
